cbd74983a635dc4936d2eba97626a4aa6dedc41c
Lorentious <72303448+Lorentious@users.noreply.github.com>
2025-05-29 22:52:05 +0200

    Update __init__.py

--- custom_components/json-ha/__init__.py
+++ custom_components/json-ha/__init__.py
@@ -4,12 +4,10 @@ import logging
 _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass, entry):
+    """Set up json_ha from a config entry."""
     ip = entry.data["ip_address"]
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = {
-        "ip": ip,
-        "groups": entry.data["selected_groups"],
-        "interval": entry.data.get("interval", 30),
-        "name": entry.data["name"]
+        "ip": ip
     }
 
     hass.async_create_task(
@@ -18,6 +16,7 @@ async def async_setup_entry(hass, entry):
     return True
 
 async def async_unload_entry(hass, entry):
+    """Unload a config entry."""
     await hass.config_entries.async_forward_entry_unload(entry, "sensor")
     hass.data[DOMAIN].pop(entry.entry_id)
     return True
