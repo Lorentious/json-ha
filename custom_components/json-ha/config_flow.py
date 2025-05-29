d20e6e257e3d214cc91eba6dad963154037e98c2
Lorentious <72303448+Lorentious@users.noreply.github.com>
2025-05-29 21:10:30 +0200

    Create config_flow.py

---
+++ custom_components/json-ha/config_flow.py
@@ -0,0 +1,16 @@
+from homeassistant import config_entries
+import voluptuous as vol
+from .const import DOMAIN
+
+class MyComponentConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle the config flow."""
+
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title="My Device", data=user_input)
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({
+                vol.Required("ip_address"): str
+            })
+        )
