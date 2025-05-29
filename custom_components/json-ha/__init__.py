081807230ed9c6fa683b5c755c11d6579faeabcc
Lorentious <72303448+Lorentious@users.noreply.github.com>
2025-05-29 21:28:14 +0200

    Update __init__.py

--- custom_components/json-ha/__init__.py
+++ custom_components/json-ha/__init__.py
@@ -3,20 +3,20 @@ import logging
 
 _LOGGER = logging.getLogger(__name__)
 
-async def async_setup(hass, config):
-    """Wird nicht verwendet (wir nutzen config entries)."""
-    return True
-
 async def async_setup_entry(hass, entry):
-    """Setup bei UI-Konfiguration."""
-    _LOGGER.info("Starte mit IP: %s", entry.data["ip_address"])
+    """Set up json_ha from a config entry."""
+    ip = entry.data["ip_address"]
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = {
+        "ip": ip
+    }
 
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = entry.data
-
-    # Hier kannst du z. B. eine Verbindung aufbauen oder eine Plattform starten
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
     return True
 
 async def async_unload_entry(hass, entry):
-    """Wird aufgerufen, wenn die Integration entfernt wird."""
-    hass.data[DOMAIN].pop(entry.entry_id, None)
+    """Unload a config entry."""
+    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    hass.data[DOMAIN].pop(entry.entry_id)
     return True
