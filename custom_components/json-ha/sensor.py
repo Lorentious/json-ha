5f981e458931248f951cce98211f499da60af969
Lorentious <72303448+Lorentious@users.noreply.github.com>
2025-05-30 11:54:19 +0200

    Update sensor.py

--- custom_components/json-ha/sensor.py
+++ custom_components/json-ha/sensor.py
@@ -1,48 +1,68 @@
 from homeassistant.helpers.entity import Entity
-from homeassistant.const import CONF_NAME
-import requests
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
 import logging
+from .const import DOMAIN
+from homeassistant.helpers.event import async_track_time_interval
+from datetime import timedelta
 
 _LOGGER = logging.getLogger(__name__)
 
-def flatten_json(y, parent_key='', sep='_'):
-    items = {}
-    for k, v in y.items():
-        new_key = f"{parent_key}{sep}{k}" if parent_key else k
+def flatten_keys(d, parent_key=""):
+    """Rekursiv alle Keys aus dict holen, mit Punkt-Notation"""
+    items = []
+    for k, v in d.items():
+        new_key = f"{parent_key}.{k}" if parent_key else k
         if isinstance(v, dict):
-            items.update(flatten_json(v, new_key, sep=sep))
+            items.extend(flatten_keys(v, new_key))
         else:
-            items[new_key] = v
+            items.append(new_key)
     return items
 
-def setup_platform(hass, config, add_entities, discovery_info=None):
-    url = config.get("url")
-    name = config.get(CONF_NAME, "JSON Sensor")
+def get_value_from_path(data, path):
+    """Hole Wert anhand Pfad mit Punkt-Notation"""
+    for part in path.split("."):
+        data = data.get(part, {})
+    return data if not isinstance(data, dict) else None
 
-    try:
-        response = requests.get(url, timeout=5)
-        response.raise_for_status()
-        json_data = response.json()
-    except Exception as e:
-        _LOGGER.error(f"Failed to fetch data from {url}: {e}")
-        return
+async def async_setup_entry(hass, entry, async_add_entities):
+    ip = entry.data["ip_address"]
+    name = entry.data["name"]
+    update_interval = entry.data.get("update_interval", 60)
+    selected_groups = entry.data["selected_groups"]
 
-    # Daten flach machen
-    flat = flatten_json(json_data)
+    entities = []
+    session = async_get_clientsession(hass)
 
-    sensors = []
-    for key, value in flat.items():
-        sensors.append(JsonSensor(name, key, value, url))
+    # Für jede Gruppe alle Keys holen, Sensoren erzeugen
+    for group in selected_groups:
+        url = f"http://{ip}/"
+        try:
+            async with session.get(url, timeout=5) as resp:
+                data = await resp.json()
+            sbi = data.get("SBI", {})
+            group_data = sbi.get(group, {})
+            keys = flatten_keys(group_data, group)
+            for key in keys:
+                entities.append(JsonHaSensor(hass, name, group, key, ip, update_interval))
+        except Exception as e:
+            _LOGGER.error(f"Fehler beim Abrufen der JSON-Daten für Gruppe {group}: {e}")
 
-    add_entities(sensors, True)
+    async_add_entities(entities, True)
 
-
-class JsonSensor(Entity):
-    def __init__(self, base_name, key, value, url):
-        self._name = f"{base_name} {key}"
-        self._state = value
+class JsonHaSensor(Entity):
+    def __init__(self, hass, base_name, group, key, ip, update_interval):
+        self._hass = hass
+        self._base_name = base_name
+        self._group = group
         self._key = key
-        self._url = url
+        self._ip = ip
+        self._state = None
+        self._unsub_update = None
+        self._update_interval = timedelta(seconds=update_interval)
+
+        # Name: <base_name> <group> <key> (key ohne group, nur letzter Teil)
+        key_short = key[len(group)+1:] if key.startswith(group + ".") else key
+        self._name = f"{base_name} {group} {key_short}"
 
     @property
     def name(self):
@@ -52,12 +72,36 @@ class JsonSensor(Entity):
     def state(self):
         return self._state
 
-    def update(self):
+    @property
+    def unique_id(self):
+        # IP + Gruppe + kompletter key
+        return f"{self._ip}_{self._group}_{self._key}"
+
+    @property
+    def should_poll(self):
+        return False
+
+    async def async_added_to_hass(self):
+        self._unsub_update = async_track_time_interval(
+            self._hass, self.async_update, self._update_interval
+        )
+        await self.async_update()
+
+    async def async_will_remove_from_hass(self):
+        if self._unsub_update:
+            self._unsub_update()
+            self._unsub_update = None
+
+    async def async_update(self, now=None):
+        url = f"http://{self._ip}/"
+        session = async_get_clientsession(self._hass)
         try:
-            response = requests.get(self._url, timeout=5)
-            response.raise_for_status()
-            json_data = response.json()
-            flat = flatten_json(json_data)
-            self._state = flat.get(self._key)
+            async with session.get(url, timeout=5) as resp:
+                data = await resp.json()
+            sbi = data.get("SBI", {})
+            # Wert anhand key abrufen
+            self._state = get_value_from_path(sbi, self._key)
+            self.async_write_ha_state()
         except Exception as e:
-            _LOGGER.error(f"Failed to update data from {self._url}: {e}")
+            _LOGGER.error(f"Fehler beim Abrufen der JSON-Daten: {e}")
+            self._state = None
